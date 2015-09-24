b72d667ba1be524f3b9681e4de744d98689d368d
Kris Jordan <krisjordan@gmail.com>
2015-09-24 11:45:14 -0400

    Add starter code for lecture 11.

---
+++ src/com/comp110/lecture11/BlackjackSimulator.java
@@ -0,0 +1,28 @@
+/*
+ * Blackjack Simulator calculates the odds of drawing blackjack
+ * from a fair deck of 52 shuffled cards by simulating trials.
+ */
+package com.comp110.lecture11;
+
+import com.comp110.cards.Deck;
+import com.comp110.cards.Card;
+
+class BlackjackSimulator {
+  public static void main(String[] args) {
+    
+      Deck deck = new Deck(); // Initialize a new 52 Card Deck
+      deck.shuffle();
+      
+      Card card0, card1;
+      card0 = deck.draw();    // Draw cards from deck
+      card1 = deck.draw();
+      
+      boolean isBlackjack = card0.isAce() && card1.getOrdinal() >= 10;
+      isBlackjack = isBlackjack || card0.getOrdinal() >= 10 && card1.isAce();
+   
+      if(isBlackjack) {
+        System.out.println("Blackjack!" + card0 + " " + card1);
+      }
+      
+  }
+}
---
+++ src/com/comp110/lecture11/RamsesRaffle.java
@@ -0,0 +1,35 @@
+package com.comp110.lecture11;
+
+import java.util.Scanner;
+
+public class RamsesRaffle {
+
+  public static void main(String[] args) throws InterruptedException {
+    Scanner keyboard = new Scanner(System.in);
+
+    System.out.println("RAMSES' RAFFLE\nWhat is up for grabs?");
+    String raffle = keyboard.nextLine();
+    
+    String person0 = "[YOUR NAME HERE]";
+    String person1 = "[FRIEND NAME HERE]";
+    String person2 = "Kris";
+    
+    int winningNumber = (int)(Math.random() * 3.0);
+    String winner;
+    if(winningNumber == 0) {
+      winner = person0;
+    } else if(winningNumber == 1) {
+      winner = person1;
+    } else {
+      winner = person2;
+    }
+    System.out.println("RAMSES DECIDES...");
+    Thread.sleep(1000); // "Sleep" for 1000 milliseconds...
+    // The suspense builds...
+    
+    System.out.println(winner + " IS THE CHOSEN ONE FOR " + raffle + "!");
+    
+    
+  }
+
+}
